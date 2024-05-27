cc2d37356bbb4cc632a609487b8ba8dd4934019f
MarioCampelloRodes <mcampellor@esimurcia.com>
2024-05-27 13:19:20 +0200

    Controls Mapping View

--- Assets/Code/Scripts/UI/OptionsScreen.cs
+++ Assets/Code/Scripts/UI/OptionsScreen.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class OptionsScreen : MonoBehaviour
 {
+    public GameObject keyboardMapping, gamepadMapping;
     private void Update()
     {
         if (Input.GetButtonDown("Escape"))
@@ -26,5 +28,31 @@ public class OptionsScreen : MonoBehaviour
     public void GoToCredits()
     {
         SceneManager.LoadScene("Credits");
+    }
+
+    public void ShowKeyboardMapping()
+    {
+        if (!keyboardMapping.activeInHierarchy)
+        {
+            keyboardMapping.SetActive(true);
+        }
+
+        if (gamepadMapping.activeInHierarchy)
+        {
+            gamepadMapping.SetActive(false);
+        }
+    }
+
+    public void ShowGamepadMapping()
+    {
+        if (!gamepadMapping.activeInHierarchy)
+        {
+            gamepadMapping.SetActive(true);
+        }
+
+        if (keyboardMapping.activeInHierarchy)
+        {
+            keyboardMapping.SetActive(false);
+        }
     }
 }
